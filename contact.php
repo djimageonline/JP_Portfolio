0722b04d353acf01ce46965fe1c69f5e93f1504e
Johnny Proano <djimageonline@gmail.com>
2022-12-08 02:33:35 -0500

    correct @media for project pics and alignment.

--- contact.php
+++ contact.php
@@ -1,23 +1,22 @@
 
 <?php
-// Get data from form 
+// Get data from form
+$mail_host = "johnnyproano.com"; 
 $firstname = $_POST['first-name'];
 $lastname = $_POST['last-name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
  
 $to = "jproano22@yahoo.com";
-$subject = "You got an email from your Dev Site";
+$subject = "New Form Submission from Dev Site";
  
 // The following text will be sent
 // Name = user entered name
 // Email = user entered email
 // Message = user entered message
-$txt ="First Name = ". $firstname . "\r\n Last Name = ". $lastname . "\r\n   Email = "
-    . $email . "\r\n Message =" . $message;
+$txt ="First Name = ". $firstname . "\r\n Last Name = ". $lastname . "\r\n   Email = ". $email . "\r\n Message =" . $message;
  
-$headers = "From: noreply@demosite.com" . "\r\n" .
-            "CC: somebodyelse@example.com";
+$headers = "From: $email" . "\r\n" .;
 if($email != NULL) {
     mail($to, $subject, $txt, $headers);
 }
