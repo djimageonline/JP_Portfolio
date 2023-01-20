f0be15568ca05e6c6bcead875ebd1f264d1eb652
Johnny Proano <djimageonline@gmail.com>
2023-01-20 00:43:40 -0500

    added php change to test

--- contact.php
+++ contact.php
@@ -1,26 +1,23 @@
 
 <?php
 // Get data from form
-$mail_host = "johnnyproano.com"; 
+// $mail_host = "johnnyproano.com"; 
 $firstname = $_POST['first-name'];
 $lastname = $_POST['last-name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
  
-$to = "jproano22@yahoo.com";
 $subject = "New Form Submission from Dev Site";
- 
-// The following text will be sent
-// Name = user entered name
-// Email = user entered email
-// Message = user entered message
+$to = "jproano22@yahoo.com";
+
 $txt ="First Name = ". $firstname . "\r\n Last Name = ". $lastname . "\r\n   Email = ". $email . "\r\n Message =" . $message;
  
-$headers = "From: $email" . "\r\n" .;
-if($email != NULL) {
-    mail($to, $subject, $txt, $headers);
-}
- 
+$headers = "From: $to \r\n";
+$headers .= "Reply-To: $email \r\n";
+
+mail($to, $subject, $txt, $headers);
+
+
 // Redirect to
-header("Location:index.html");
+header("Location: index.html");
 ?>
